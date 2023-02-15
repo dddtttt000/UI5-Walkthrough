28cfa3a00639c296373c667f5d8d7415bdf1d47c
songha <dddtttt000@gmail.com>
2023-02-15 12:44:20 +0900

    Step 3: Controls

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // application logic
-sap.ui.define([], function () {
+sap.ui.define(["sap/m/Text"], function (Text) {
   "use strict"
 
-  alert("UI5 is ready")
+  new Text({
+    text: "Hello World~~",
+  }).placeAt("content")
 })
