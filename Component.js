57a23797ca2d3a9a4537d12e18a3a4f672ade377
songha <dddtttt000@gmail.com>
2023-02-17 12:07:55 +0900

    Step 36: Content Density

--- Component.js
+++ Component.js
@@ -3,8 +3,9 @@ sap.ui.define(
     "sap/ui/core/UIComponent",
     "sap/ui/model/json/JSONModel",
     "sap/ui/model/resource/ResourceModel",
+    "sap/ui/Device",
   ],
-  function (UIComponent, JSONModel, ResourceModel) {
+  function (UIComponent, JSONModel, Device) {
     "use strict"
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
       metadata: {
@@ -23,8 +24,23 @@ sap.ui.define(
         var oModel = new JSONModel(oData)
         this.setModel(oModel)
 
+        // set device model
+        var oDeviceModel = new JSONModel(Device)
+        oDeviceModel.setDefaultBindingMode("OneWay")
+        this.setModel(oDeviceModel, "device")
+
         // create the views based on the url/hash
         this.getRouter().initialize()
+      },
+      getContentDensityClass: function () {
+        if (!this._sContentDensityClass) {
+          if (!Device.support.touch) {
+            this._sContentDensityClass = "sapUiSizeCompact"
+          } else {
+            this._sContentDensityClass = "sapUiSizeCozy"
+          }
+        }
+        return this._sContentDensityClass
       },
     })
   }
