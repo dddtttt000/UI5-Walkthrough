580e7775dcd0fe7684e9685cd6c23f3445b98313
songha <dddtttt000@gmail.com>
2023-02-21 14:49:31 +0900

    add pdf view and sign pad

---
+++ controller/PDFView.controller.js
@@ -0,0 +1,73 @@
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/routing/History",
+  ],
+  function (Controller, JSONModel, History) {
+    "use strict"
+
+    return Controller.extend("sap.ui.demo.walkthrough.controller.PDFView", {
+      onInit: function () {
+        this.getView().setModel(
+          new JSONModel({
+            // pdfsource: ""
+            pdfSource: "static/OpenUI5.pdf",
+          })
+        )
+      },
+      onFileChange: function (oEvent) {
+        var reader = new FileReader(),
+          me = this
+        reader.onload = function (oEvent) {
+          me.getView()
+            .getModel()
+            .setProperty("/pdfsource", oEvent.target.result)
+        }
+        reader.readAsDataURL(oEvent.getParameter("files")[0])
+      },
+      onOpenPDFViewer: function (oEvent) {
+        if (!this._oDialog) {
+          this._oDialog = sap.ui.xmlfragment(
+            "be.wl.PDFExample.view.dialog.PDFViewer",
+            this
+          )
+          this.getView().addDependent(this._oDialog)
+        }
+        this._oDialog.open()
+      },
+      onShowPDF: function () {
+        this._sValidPath = "/model/sample.pdf"
+        this._sInvalidPath = sap.ui.require.toUrl(
+          "sap/m/sample/PDFViewerEmbedded/sample_nonexisting.pdf"
+        )
+        this._oModel = new JSONModel({
+          Source: this._sValidPath,
+          Title: "PDF view now",
+          Height: "600px",
+        })
+        this.getView().setModel(this._oModel)
+      },
+
+      onCorrectPathClick: function () {
+        this._oModel.setProperty("/Source", this._sValidPath)
+      },
+
+      onIncorrectPathClick: function () {
+        this._oModel.setProperty("/Source", this._sInvalidPath)
+      },
+
+      onNavBack: function () {
+        var oHistory = History.getInstance()
+        var sPreviousHash = oHistory.getPreviousHash()
+
+        if (sPreviousHash !== undefined) {
+          window.history.go(-1)
+        } else {
+          var oRouter = this.getOwnerComponent().getRouter()
+          oRouter.navTo("overview", {}, true)
+        }
+      },
+    })
+  }
+)
--- controller/PdfPanel.controller.js
+++ controller/PdfPanel.controller.js
@@ -1,35 +1,20 @@
 sap.ui.define(
   [
     "sap/ui/core/mvc/Controller",
-    // "sap/ui/core/Fragment",
     "sap/ui/model/json/JSONModel",
+    "sap/ui/core/Fragment",
   ],
   function (Controller, JSONModel) {
     "use strict"
 
     return Controller.extend("sap.ui.demo.walkthrough.controller.PdfPanel", {
-      onShowPDF: function () {
-        // this._sValidPath = sap.ui.require.toUrl("./model/sample.pdf")
-        this._sValidPath =
-          "https://github.com/dddtttt000/UI5-Walkthrough/blob/main/model/sample.pdf"
-        console.log("this._sValidPath ===>", this._sValidPath)
-        this._sInvalidPath = sap.ui.require.toUrl(
-          "sap/m/sample/PDFViewerEmbedded/sample_nonexisting.pdf"
-        )
-        this._oModel = new JSONModel({
-          Source: this._sValidPath,
-          Title: "PDF view now",
-          Height: "600px",
+      onInit: function () {
+        var oViewModel = new JSONModel({
+          currency: "EUR",
         })
-        this.getView().setModel(this._oModel)
-      },
-
-      onCorrectPathClick: function () {
-        this._oModel.setProperty("/Source", this._sValidPath)
-      },
-
-      onIncorrectPathClick: function () {
-        this._oModel.setProperty("/Source", this._sInvalidPath)
+        this.getView().setModel(oViewModel, "view")
+        var oRouter = this.getOwnerComponent().getRouter()
+        oRouter.getRoute("PDFView")
       },
 
       onShowViewImg: function () {
@@ -47,6 +32,11 @@ sap.ui.define(
         // note: We don't need to chain to the pDialog promise, since this event-handler
         // is only called from within the loaded dialog itself.
         this.byId("PdfDialog").close()
+      },
+
+      onPress: function () {
+        var oRouter = this.getOwnerComponent().getRouter()
+        oRouter.navTo("PDFView")
       },
     })
   }
